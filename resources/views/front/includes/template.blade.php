b9f64e05d0462c5b5956933295694c4689d3979b
Edward Reyes <43286295+edw-rys@users.noreply.github.com>
2021-06-30 19:27:25 -0500

    images

--- resources/views/front/includes/template.blade.php
+++ resources/views/front/includes/template.blade.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>{{ config('app_project.title')}}</title>
-	<meta name="description" content="{{ config('app_project.description') }}">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<link rel="shortcut icon" type="image/x-icon" href="{{ asset('front/images/me/gafas.jpg')}}">
 
-	<!-- STYLESHEETS -->
-	<link rel="stylesheet" href="{{ asset_front('css/bootstrap.min.css') }}" type="text/css" media="all">
-	<link rel="stylesheet" href="{{ asset_front('css/all.min.css') }}" type="text/css" media="all">
-	<link rel="stylesheet" href="{{ asset_front('css/simple-line-icons.css') }}" type="text/css" media="all">
-	<link rel="stylesheet" href="{{ asset_front('css/slick.css') }}" type="text/css" media="all">
-	<link rel="stylesheet" href="{{ asset_front('css/animate.css') }}" type="text/css" media="all">
-	<link rel="stylesheet" href="{{ asset_front('css/magnific-popup.css') }}" type="text/css" media="all">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>{{ config('app_project.title') }}</title>
+    <meta name="description" content="{{ config('app_project.description') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('front/images/me/gafas.jpg') }}">
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="{{ asset_front('css/bootstrap.min.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset_front('css/all.min.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset_front('css/simple-line-icons.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset_front('css/slick.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset_front('css/animate.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset_front('css/magnific-popup.css') }}" type="text/css" media="all">
     <link rel="stylesheet" href="{{ asset_front('css/style.css') }}" type="text/css" media="all">
     {{-- @notifyCss --}}
     @yield('links')
     @yield('scripts_head')
-	<!--[if lt IE 9]>
+    <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
@@ -27,65 +28,66 @@
 
 <body>
 
-<!-- Preloader -->
-<div id="preloader">
-	<div class="outer">
-		<!-- Google Chrome -->
-		<div class="infinityChrome">
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="outer">
+            <!-- Google Chrome -->
+            <div class="infinityChrome">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
 
-		<!-- Safari and others -->
-		<div class="infinity">
-			<div>
-				<span></span>
-			</div>
-			<div>
-				<span></span>
-			</div>
-			<div>
-				<span></span>
-			</div>
-		</div>
-		<!-- Stuff -->
-		<svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="goo-outer">
-			<defs>
-				<filter id="goo">
-					<feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
-					<feColorMatrix in="blur" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
-					<feBlend in="SourceGraphic" in2="goo" />
-				</filter>
-			</defs>
-		</svg>
-	</div>
-</div>
+            <!-- Safari and others -->
+            <div class="infinity">
+                <div>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+            </div>
+            <!-- Stuff -->
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="goo-outer">
+                <defs>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
+                        <feColorMatrix in="blur" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                        <feBlend in="SourceGraphic" in2="goo" />
+                    </filter>
+                </defs>
+            </svg>
+        </div>
+    </div>
 
-<!-- mobile header -->
-<header class="mobile-header-1">
-	<div class="container">
-		<!-- menu icon -->
-		<div class="menu-icon d-inline-flex mr-4">
-			<button>
-				<span></span>
-			</button>
-		</div>
-		<!-- logo image -->
-		<div class="site-logo">
-			<a href="/">
-				<img src="{{ img_me() }}" alt="Edw" />
-			</a>
-		</div>
-	</div>
-</header>
+    <!-- mobile header -->
+    <header class="mobile-header-1">
+        <div class="container">
+            <!-- menu icon -->
+            <div class="menu-icon d-inline-flex mr-4">
+                <button>
+                    <span></span>
+                </button>
+            </div>
+            <!-- logo image -->
+            <div class="site-logo">
+                <a href="/">
+                    <img src="{{ img_me() }}" alt="Edw" />
+                </a>
+            </div>
+        </div>
+    </header>
 
     @include('front.components.sidebar')
 
     <!-- main layout -->
     <main class="content">
+		<img src="{{ asset('files/images/my/samoyed_icon.png') }}" alt="My samoyedo" class="my-samoyed-r-b">
         @yield('section')
-        
+
     </main>
     {{-- @notifyJs --}}
 
@@ -114,4 +116,5 @@
     @yield('scripts_body_after')
 
 </body>
+
 </html>
