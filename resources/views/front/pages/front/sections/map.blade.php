f3cfd74f3e461d0c775291478c155bb7bcfc8d8e
Edward Reyes <43286295+edw-rys@users.noreply.github.com>
2022-10-10 15:43:36 -0500

    resources\views\front\pages\front\sections\map.blade.php

--- resources/views/front/pages/front/sections/map.blade.php
+++ resources/views/front/pages/front/sections/map.blade.php
@@ -1,6 +1,11 @@
 <div style="overflow: auto;width: 100%">
     <div id="regions_div" style="width: 100%; height: 500px;"></div>
 </div>
+<ol>
+    @foreach ($data as $item_country)
+        <li>{{ $item_country[0]  }} => {{ $item_country[1] }}</li>
+    @endforeach
+</ol>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
     google.charts.load('current', {
